41f9564462cfca86752a2ccac45a535492ef03d5
Scott Laur <scottmlaur@gmail.com>
2025-06-30 16:44:40 -0400

    Moved all files to root, rewrote index.html, confirmed working asset paths

--- game.js
+++ game.js
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>SofieBurn</title>
-  <link rel="stylesheet" href="intro.css" />
-</head>
-<body>
-  <div id="intro-screen">
-    <h1>SofieBurn</h1>
-    <img src="assets/intro.png" alt="Sofie holding candle" id="intro-image" />
-    <button id="start-button">Light the Flame</button>
-  </div>
-
-  <canvas id="game-canvas" style="display: none;"></canvas>
-
-  <script src="game.js"></script>
-</body>
-
 <body>
   <div id="intro-screen">
     <h1>SofieBurn</h1>
